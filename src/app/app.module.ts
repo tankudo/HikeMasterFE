8b1af4d09fa226111e5a4cf29dea6ad023b1ab7c
Varadi Tamas <varadi.tomy@gmail.com>
2020-07-14 11:15:15 +0200

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HideMasterComponent } from './components/hike-master/hide-master.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HideMasterComponent
   ],
   imports: [
     BrowserModule,
