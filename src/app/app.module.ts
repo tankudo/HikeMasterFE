6ea895f5a1fb9b242a9b54078afc98b509f81fff
KoFanni <fankov96@gmail.com>
2020-07-14 11:35:43 +0200

    add css files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HideMasterComponent } from './components/hike-master/hide-master.component';
+import { TourListComponent } from './components/tour-list/tour-list.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HideMasterComponent
+    HideMasterComponent,
+    TourListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/hike-master/hide-master.component.ts
+++ src/app/components/hike-master/hide-master.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-hide-master',
   templateUrl: './hike-master.component.html',
-  styleUrls: ['./hide-master.component.scss']
+  styleUrls: ['./hike-master.component.scss']
 })
 export class HideMasterComponent implements OnInit {
 
