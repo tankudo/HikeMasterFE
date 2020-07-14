63cef1d8eb32c8421cd575e1c874c1eec4409629
“tankudo” <ankudo@icloud.com>
2020-07-14 11:54:20 +0200

    need ng bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {LoginComponent} from './components/login/login.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'hikemaster';
+
+  constructor(private modalService: NgbModal) {
+  }
+
+  ngOnInit() {
+    const modalRef = this.modalService.open(LoginComponent);
+  }
 }
