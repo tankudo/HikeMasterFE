c774fc1be0121097f85209128fdde34355d909fd
KoFanni <fankov96@gmail.com>
2020-07-16 12:08:25 +0200

    tour-list component is working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {FrontpageComponent} from './components/frontpage/frontpage.component';
 import {TourViewComponent} from "./components/tour-view/tour-view.component";
+import {TourListComponent} from "./components/tour-list/tour-list.component";
 
 
 
 const routes: Routes = [
   {path: 'frontpage', component: FrontpageComponent},
-  {path: '', pathMatch: 'full', redirectTo: 'frontpage'}];
+  {path: '', pathMatch: 'full', redirectTo: 'frontpage'},
+  {path: 'tour-list', component: TourListComponent},
+  {path: '', pathMatch: 'full', redirectTo: 'tour-list'}];
  // {path: 'tour-view', component: TourViewComponent},
  // {path: '', pathMatch: 'full', redirectTo: 'tour-view'}];
 
