5a3bad490edb624774f7c780ccd0ab88262450c8
“tankudo” <ankudo@icloud.com>
2020-07-30 23:07:22 +0200

    tests are fixed

--- src/app/components/contact/contact-modal/contact-modal.component.spec.ts
+++ src/app/components/contact/contact-modal/contact-modal.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ContactModalComponent } from './contact-modal.component';
+import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 
 describe('ContactModalComponent', () => {
   let component: ContactModalComponent;
@@ -8,7 +9,8 @@ describe('ContactModalComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ContactModalComponent ]
+      declarations: [ ContactModalComponent ],
+      providers: [ NgbActiveModal ]
     })
     .compileComponents();
   }));
